eec8924b81d032351eef7d887e29962758a28e39
cruzlorite <josemariacruzlorite@gmail.com>
2025-09-20 15:28:36 +0200

    Reset version to 0.1.0 for fresh start
    - Reset version in pyproject.toml to 0.1.0
    - Reset version in bdns/fetch/__init__.py to 0.1.0
    - All tags have been removed from local and remote
    - Starting fresh development cycle

--- bdns/fetch/__init__.py
+++ bdns/fetch/__init__.py
@@ -1,6 +1,6 @@
 """BDNS Fetch - Base de Datos Nacional de Subvenciones (BDNS) Client."""
 
-__version__ = "3.1.7"
+__version__ = "0.1.0"
 
 from bdns.fetch.utils import smart_open, format_date_for_api_request, format_url
 from bdns.fetch.types import (
